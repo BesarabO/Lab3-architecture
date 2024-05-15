17dc2c58ed834b608673cf2d7c726833e3752150
BesarabO <rurikchannel@gmail.com>
2024-05-15 22:34:18 +0300

    Add files via upload

---
+++ Lab3/ПТ-23-1/Besarab Oleksandr/batch/openzvit.bat
@@ -0,0 +1,3 @@
+@echo off
+chcp 65001 > nul
+start /wait winword lab3-звіт.docx
---
+++ Lab3/ПТ-23-1/Besarab Oleksandr/batch/part2-1.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "D:\Бесараб Олександр\Архітектура\Lab3\ПТ-23-1\Besarab Oleksandr\batch"
+attrib -h *.* /s /d
